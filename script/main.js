ed24d129394ea7047fd778d843c60a0120ed79c5
Khalil Gibran <zidangibran14@gmail.com>
2024-04-03 15:43:33 +0700

    Update main.js

--- script/main.js
+++ script/main.js
@@ -19,31 +19,59 @@
 }); 
 */
 
-window.addEventListener('load', async () => {
-    const { value: password } = await Swal.fire({
-      title: "Pesan Rahasia, masukin password nya!",
-      input: "text",
-      inputLabel: "Masukkin password",
-      // inputValue,
-      showCancelButton: true,
-      inputValidator: (value) => {
-        if (value != "0414" && value != "414" && value != "18") {
-          return "Clue nya tanggal ultah ibiy + tanggal ultah iban";
-        }
+// window.addEventListener('load', async () => {
+    // Set the date we're counting down to
+    const countDownDate = new Date("Apr 3, 2024 15:40:00").getTime();
+    
+    // Update the count down every 1 second
+    const x = setInterval(function() {
+
+      // Get today's date and time
+      const now = new Date().getTime();
+        
+      // Find the distance between now and the count down date
+      const distance = countDownDate - now;
+        
+      // Time calculations for days, hours, minutes and seconds
+      const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+      // Output the result in an element with id="demo"
+      document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+      + minutes + "m " + seconds + "s ";
+    
+      // If the count down is over, write some text 
+      if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("demo").innerHTML = "";
+          const { value: password } = await Swal.fire({
+              title: "Pesan Rahasia, masukin password nya!",
+              input: "text",
+              inputLabel: "Masukkin password",
+              // inputValue,
+              showCancelButton: true,
+              inputValidator: (value) => {
+                if (value != "0414" && value != "414" && value != "18") {
+                  return "Clue nya tanggal ultah ibiy + tanggal ultah iban";
+                }
+              }
+            });
+            if (password == "0414" || password == "414") {
+                Swal.fire({
+                  title: `Yeay.. password bener, met nonton..`,
+                  showConfirmButton: false,
+                  timer: 1500,
+                  timerProgressBar: true,
+                }).then( (result) => {
+                    document.querySelector('.song').play();
+                    animationTimeline();
+                })
+            }
       }
-    });
-    if (password == "0414" || password == "414") {
-        Swal.fire({
-          title: `Yeay.. password bener, met nonton..`,
-          showConfirmButton: false,
-          timer: 1500,
-          timerProgressBar: true,
-        }).then( (result) => {
-            document.querySelector('.song').play();
-            animationTimeline();
-        })
-    }
-}); 
+    }, 1000);
+// }); 
 
 
 // animation timeline
